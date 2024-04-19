1b5ff90f3636b6359dfc723df8c80a7b50c95421
Mats Brorsson <mats@embeinnovation.com>
2024-04-19 06:38:10 +0200

    wip
    Signed-off-by: Mats Brorsson <mats@embeinnovation.com>

--- service1/src/main.rs
+++ service1/src/main.rs
@@ -1,9 +1,6 @@
 #[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
 
-use rocket::serde::{Deserialize, Serialize};
-use rocket_contrib::json::Json;
-
+use rocket::serde::{Deserialize, Serialize, json::Json, json};
 
 #[cfg(test)] mod tests;
 
@@ -35,6 +32,7 @@ struct ResultValue {
 }
 
 #[derive(Deserialize,Serialize)]
+#[serde(crate = "rocket::serde")]
 struct Args {
     arg1: i64,
     arg2: i64,
@@ -48,17 +46,26 @@ fn sub(arg1: i64, arg2: i64) -> i64 {
     arg1 - arg2
 }
 
-#[post("/math/<op>", format = "json", data = "<args>")]
+#[post("/math/<op>",  data = "<args>")] 
 fn math(op: & str, args: Json<Args>) -> Json<ResultValue> {
+    print!("{} {} {}", op, args.arg1, args.arg2);
     match op {
         "add" => Json(ResultValue { result: add(args.arg1, args.arg2) }),
         "sub" => Json(ResultValue { result: sub(args.arg1, args.arg2) }),
+        _ => Json(ResultValue { result: 0 }),
     }
 }
 
+#[post("/add", format = "text", data = "<args>")] 
+fn adder(args: Json<Args>) -> Json<ResultValue> {   
+    print!("{} {}", args.arg1, args.arg2);
+    Json(ResultValue { result: add(args.arg1, args.arg2) })
+}
+
+
 // Try visiting:
 //   http://127.0.0.1:8000/usage
-#[get("/usage", format = "json")]
+#[get("/", format = "json")]
 fn usage() -> Json<ResultMessage> {
     Json(ResultMessage { message: format!("This is how you use this service!") })
 }
@@ -67,6 +74,7 @@ fn usage() -> Json<ResultMessage> {
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![usage])
+        .mount("/add", routes![adder])
         .mount("/math", routes![math])
         
 }
